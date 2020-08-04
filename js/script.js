c3ebbf82e34bf57b2139177cf6ac3ed3dc9232c2
Luka <luanafernandes73@gmail.com>
2020-08-04 12:49:47 -0400

    adjust form validation

--- js/script.js
+++ js/script.js
@@ -2,8 +2,31 @@ $('.carousel').carousel({
     interval: 1800
 });
 
-function claer() {
-  document.getElementById("form").reset();
+
+function validade() {
+  var flag = false;
+  var forms = document.getElementsByClassName('needs-validation');
+  var validation = Array.prototype.filter.call(forms, function(form) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else { flag = true; }
+      
+      form.classList.add('was-validated');
+    });
+  
+    if(flag) {
+      var firstName = $(".first-name").val();
+      var lastName = $(".last-name").val();
+      var email = $(".email").val();
+      var address = $(".address").val();
+      var alert =  $('.alert');
+
+      var msg = 'First Name: ' + firstName + ' Last Name: ' + lastName+ 'Email:' + email +
+      ' Address : ' + address + '. Confirmed Client added ';
+      alert.text(msg);
+      alert.removeAttr("hidden");
+    }
 }
 
 $(document).ready(function() {
