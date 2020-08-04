a5ba1373b45107d79f7c4a711bb8344880ced067
Luka <luanafernandes73@gmail.com>
2020-08-04 18:51:07 -0400

    add loan calculation

--- js/script.js
+++ js/script.js
@@ -1,47 +1,78 @@
+$(document).ready(function() {
+  var selectedClass = "1";
+  $("#gallery").show();
+  $("#gallery div").not("."+selectedClass).hide().removeClass('animation');
+
+  $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").show();
+    $("#gallery div").not("."+selectedClass).hide().removeClass('animation');
+    setTimeout(function() {
+      $("."+selectedClass).show().addClass('animation');
+      $("#gallery").show();
+    }, 300);
+  });
+});
+
 $('.carousel').carousel({
     interval: 1800
 });
 
 
 function validade() {
-  var flag = false;
-  var forms = document.getElementsByClassName('needs-validation');
-  var validation = Array.prototype.filter.call(forms, function(form) {
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      } else { flag = true; }
-      
-      form.classList.add('was-validated');
-    });
-  
-    if(flag) {
-      var firstName = $(".first-name").val();
-      var lastName = $(".last-name").val();
-      var email = $(".email").val();
-      var address = $(".address").val();
-      var alert =  $('.alert');
+  var form = document.getElementsByClassName('needs-validation')[0];
+  if(!form.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+  } else {
+    var requiredFields = 'First Name: ' + $(".first-name").val() + ' Last Name: ' 
+                          + $(".last-name").val()+ ' Email:' + $(".email").val();
+    var addressField = ($(".address").val()) ? ' Address : ' + $(".address").val() : "";
+    var msg = requiredFields + addressField + '. Confirmed Client added ';
 
-      var requiredFields = 'First Name: ' + firstName + ' Last Name: ' + lastName+ 'Email:' + email;
-      var addressField = (address) ? ' Address : ' + address : "";
-      var msg = requiredFields + addressField + '. Confirmed Client added ';
-      alert.text(msg);
-      alert.removeAttr("hidden");
-    }
+    $('.alert').text(msg);
+    $('.alert').removeAttr("hidden"); 
+  }
+  
+  form.classList.add('was-validated');  
 }
 
-$(document).ready(function() {
-    var selectedClass = "1";
-    $("#gallery").show();
-    $("#gallery div").not("."+selectedClass).hide().removeClass('animation');
+function reinicia() {
+  var form = document.getElementsByClassName('needs-validation')[0];
+  form.classList.remove('was-validated');
+  $('.alert').hide();
+}
 
-    $(".filter").click(function(){
-      selectedClass = $(this).attr("data-rel");
-      $("#gallery").show();
-      $("#gallery div").not("."+selectedClass).hide().removeClass('animation');
-      setTimeout(function() {
-        $("."+selectedClass).show().addClass('animation');
-        $("#gallery").show();
-      }, 300);
-    });
-});
+
+function reiniciaLoan() {
+  var form = document.getElementsByClassName('loan-validation')[0];
+  form.classList.remove('was-validated');
+  $('.alert').hide();
+}
+
+function loanAmount() {
+  var loan = $('#loan').val();
+  var years = $('#years').val();
+  var rate = $('#rate').val();
+
+  var monthlyRate = rate / (12 * 100);
+  var monthlyPayment = loan * monthlyRate / (1 - 1 / Math.pow(1 + monthlyRate, years * 12));
+  var totalPayment = monthlyPayment * 12 * years;
+  var monthly = (monthlyPayment * 100) / 100.0;
+  var total = (totalPayment * 100) / 100.0;
+ 
+  $('#monthly').val(monthly.toFixed(2));
+  $('#total').val(total.toFixed(2));
+
+}
+
+function validLoan() {
+  var form = document.getElementsByClassName('loan-validation')[0];
+  if(!form.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  
+  form.classList.add('was-validated');
+  loanAmount();  
+}
